00e0378eb98de1d38f3b06273af32baa198654e2
Tsukina-7mochi <tsukina7mochi@gmail.com>
2023-04-25 15:06:16 +0900

    change file structure

---
+++ example/example.ts
@@ -0,0 +1,22 @@
+import { JSONParser, replaceJsonValue } from "../mod.ts";
+
+const value = `{
+  "keys": {
+    "key1": {
+      "value1": 0,
+      "value2": 1,
+      "value3": 2
+    },
+    "key2": ["a", "b", "c"]
+  },
+  "text": "foo"
+}
+`;
+
+const parser = new JSONParser(value);
+const jsonTree = parser.parseJsonText();
+replaceJsonValue(jsonTree, ['keys', 'key1', 'value2'], '-1')
+replaceJsonValue(jsonTree, ['keys', 'key2', 1], '"X"')
+replaceJsonValue(jsonTree, ['text'], '"bar"')
+
+console.log(jsonTree.stringify());
--- src/mod.ts
+++ src/mod.ts
@@ -1,23 +1,2 @@
-import JSONParser from "./parser.ts";
-import { replaceJsonValue } from "./editor.ts";
-
-const value = `{
-  "keys": {
-    "key1": {
-      "value1": 0,
-      "value2": 1,
-      "value3": 2
-    },
-    "key2": ["a", "b", "c"]
-  },
-  "text": "foo"
-}
-`;
-
-const parser = new JSONParser(value);
-const jsonTree = parser.parseJsonText();
-replaceJsonValue(jsonTree, ['keys', 'key1', 'value2'], '-1')
-replaceJsonValue(jsonTree, ['keys', 'key2', 1], '"X"')
-replaceJsonValue(jsonTree, ['text'], '"bar"')
-
-console.log(jsonTree.stringify());
+export * from './parser.ts';
+export * from './editor.ts'
